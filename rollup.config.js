d889ad50ea9e350af36e028387b2e1748d1ff256
jensnti <jensandreasson77@gmail.com>
2021-07-22 10:54:53 +0200

    ➕node rollup 🐛confetti

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
-const builtins = require('rollup-plugin-node-builtins');
 const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
 const json = require('rollup-plugin-json');
+const nodeResolve = require('rollup-plugin-node-resolve');
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/js/main.js',
-    output: {
-        sourcemap: true,
-        format: 'iife',
-        name: 'main',
-        file: 'public/js/main.bundle.js',
-      },
-    plugins: [builtins(), nodeResolve(), commonjs(), json()]
+    output: [
+        // {
+        //     sourcemap: true,
+        //     format: 'iife',
+        //     name: 'main',
+        //     file: 'public/js/main.bundle.js'
+        // },
+        {
+            file: 'public/js/bundle.min.js',
+            format: 'iife',
+            name: 'main'
+        }
+    ],
+    plugins: [nodeResolve(), commonjs(), json(), terser()]
 };
