6962b1d39262d1511211afa40174c5f94c5086a7
jensnti <jensandreasson77@gmail.com>
2021-07-27 19:21:26 +0200

    🚸💄accordion content visible w/o js

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -6,10 +6,11 @@ const accordion = () => {
 
     accordionItems.forEach((item) => {
         if(item.querySelector('h2')) return;
+        const collapse = item.querySelector('.accordion__item-collapse');
+        collapse.classList.add('mh-0');
         item.addEventListener('click', () => {
             const button = item.querySelector('button');
             button.classList.toggle('active');
-            const collapse = item.querySelector('.accordion__item-collapse');
             if (collapse.style.maxHeight) {
                 collapse.style.maxHeight = null;
             } else {
