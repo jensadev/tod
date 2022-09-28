3d988c58967b7409720199f9676a95a52abefd73
Jens Andreasson <jensandreasson77@gmail.com>
2022-09-28 20:30:05 +0200

    ✨ svg shortcode v. classlist

--- src/shortcodes/svg.js
+++ src/shortcodes/svg.js
@@ -1,17 +1,25 @@
 // https://dev.to/extrabright/11ty-inject-svg-code-using-shortcodes-3l4m
 const fs = require('fs');
 
-const getSvgContent = (file) => {
+const getSvgContent = (file, classList) => {
     let relativeFilePath = `./src/assets/icons/${file}`;
     let data = fs.readFileSync(relativeFilePath, (err, contents) => {
         if (err) return err;
         return contents;
     });
-
-    return data.toString('utf8');
+    if (classList) {
+        return (
+            data.toString('utf8').slice(0, 4) +
+            ` class="${classList}" ` +
+            data.toString('utf8').slice(4)
+        );
+    } else {
+        return data.toString('utf8');
+    }
 };
 
 module.exports = {
+    svg: getSvgContent,
     star: () => {
         return getSvgContent('grade_FILL1_wght400_GRAD0_opsz24.svg');
     },
