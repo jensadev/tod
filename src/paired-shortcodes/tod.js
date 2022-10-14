b1b385f976cd2ff85fb96f9cd5592931e8020b6d
Jens Andreasson <jens.andreasson@ntig.se>
2022-10-14 11:57:01 +0200

    💄

--- src/paired-shortcodes/tod.js
+++ src/paired-shortcodes/tod.js
@@ -1,18 +1,23 @@
+const { star, stars } = require('../shortcodes/svg.js'); 
+
 module.exports = {
     intro: (content) => {
-        return `<section class="part__introduction">${content}</section>`;
+        return `<section class="part__introduction" role="inledning">${content}</section>`;
     },
     instruktioner: (content) => {
-        return `<section class="part__instructions">${content}</section>`;
+        return `<section class="part__instructions" role="instruktioner">${content}</section>`;
     },
-    uppgifter: (content) => {
-        return `<section class="part__assignments">${content}</section>`;
+    uppgifter: (content, lead) => {
+        return `<section class="part__assignments" role="uppgifter"><header><h2 id="uppgifter">Uppgifter</h2>${ lead ? lead : '' }</header>${content}</section>`;
+    },
+    bas: (content) => {
+        return `<div class="part__assignments-basic"><h3>${star()}</h3><div>${content}</div></div>`;
     },
     extra: (content) => {
-        return `<div class="part__assignments-extra">${content}</div>`;
+        return `<div class="part__assignments-extra"><h3>${stars()}</h3><div>${content}</div></div>`;
     },
-    facit: (content) => {
-        return `<section class="part__solution"><h2>Hjälp</h2>${content}</section>`;
+    facit: (content, title) => {
+        return `<section class="part__solution"><h2>${title}</h2>${content}</section>`;
     },
     lead: (content) => {
         return `<p class="lead">${content}</p>`;
